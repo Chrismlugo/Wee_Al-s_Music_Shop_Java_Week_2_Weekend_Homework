c273cbfed51116651ee069866c35ae9be582d83e
Chris Murphy <_chrism_@live.com>
2018-03-09 12:05:26 +0000

    tested getModel() and getInstrumentType() methods

--- src/main/java/Abstract/Instrument.java
+++ src/main/java/Abstract/Instrument.java
@@ -5,11 +5,19 @@ import Enum.InstrumentType;
 public abstract class Instrument {
     private  String model;
     private String material;
-    private Enum.InstrumentType instrumentType;
+    private InstrumentType instrumentType;
 
     public Instrument(String model, String material, InstrumentType instrumentType) {
         this.model = model;
         this.material = material;
         this.instrumentType = instrumentType;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public InstrumentType getInstrumentType() {
+        return instrumentType;
     }
 }
--- src/test/java/TestGuitar.java
+++ src/test/java/TestGuitar.java
@@ -1,14 +1,25 @@
 import Enum.InstrumentType;
 import Instruments.Guitar;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestGuitar {
     Guitar guitar;
 
     @Before
     public void setUp() throws Exception {
-        guitar = new Guitar("Gretsch", "Rich Mahogany", InstrumentType.STRING);
+        guitar = new Guitar("Explorer", "Rich Mahogany", InstrumentType.STRING);
     }
 
+    @Test
+    public void hasModel() {
+        assertEquals("Explorer", guitar.getModel());
+    }
 
+    @Test
+    public void hasInstrumentType() {
+        assertEquals(InstrumentType.STRING, guitar.getInstrumentType());
+    }
 }
